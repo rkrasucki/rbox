45857b28b5ae1c502b66d104d748b818fcc5c35b
rkrasucki <robert.krasucki@gmail.com>
2019-05-24 09:47:46 +0200

    Add POM, properties

---
+++ src/main/java/com/rkrasucki/rbox/RboxApplication.java
@@ -0,0 +1,15 @@
+package com.rkrasucki.rbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class RboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RboxApplication.class, args);
+	}
+
+}
